c8bcf469457b6a40340148da9de4857bea4d3d4b
Cooper Saye <coopersaye@gmail.com>
2022-06-21 02:04:17 -0700

    Populate index page.

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,16 @@ export default function Index() {
   }
 
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.center}>
+        <h1>Cooper Saye</h1>
+        <Link label="personal website" url="csaye.com" link="https://www.csaye.com/" />
+        <Link label="github" url="github.com/csaye" link="https://github.com/csaye" />
+        <Link label="linkedin" url="linkedin.com/in/coopersaye" link="https://www.linkedin.com/in/coopersaye/" />
+        <Link label="twitter" url="twitter.com/csaye_" link="https://twitter.com/csaye_" />
+        <Link label="soundcloud" url="soundcloud.com/csaye" link="https://soundcloud.com/csaye" />
+        <Link label="itch.io" url="csaye.itch.io" link="https://csaye.itch.io/" />
+      </div>
     </div>
   );
 }
