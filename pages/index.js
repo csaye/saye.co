51f4f028971f569b5887f75a82c6f28981ac7cad
Cooper Saye <coopersaye@gmail.com>
2022-06-21 02:03:14 -0700

    Create Link component.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
+  function Link(props) {
+    const { label, url, link } = props;
+
+    return (
+      <p>
+        {label}
+        {' • '}
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          {url}
+        </a>
+      </p>
+    );
+  }
+
   return (
     <div>
     </div>
