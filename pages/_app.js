a3992d154cbbc745f9dedb7dab470f794d6237ab
Cooper Saye <coopersaye@gmail.com>
2022-06-22 01:18:04 -0700

    Initialize fontawesome.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import Head from 'next/head';
 
+// initialize fontawesome
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 import '../styles/globals.css';
 
 export default function App(props) {
