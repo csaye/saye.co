9a226f2dab7a728fa0b46e70ea7df43640c1b9ba
Cooper Saye <coopersaye@gmail.com>
2022-06-23 01:32:00 -0700

    Remove old links.

--- pages/index.js
+++ pages/index.js
@@ -19,12 +19,6 @@ export default function Index() {
     <div className={styles.container}>
       <div className={styles.center}>
         <h1>Cooper Saye</h1>
-        <Link label="personal website" url="csaye.com" link="https://www.csaye.com/" />
-        <Link label="github" url="github.com/csaye" link="https://github.com/csaye" />
-        <Link label="linkedin" url="linkedin.com/in/coopersaye" link="https://www.linkedin.com/in/coopersaye/" />
-        <Link label="twitter" url="twitter.com/csaye_" link="https://twitter.com/csaye_" />
-        <Link label="soundcloud" url="soundcloud.com/csaye" link="https://soundcloud.com/csaye" />
-        <Link label="itch.io" url="csaye.itch.io" link="https://csaye.itch.io/" />
       </div>
     </div>
   );
